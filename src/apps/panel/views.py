ce00dba52caadf3cdfb27758d71c8a9cdf7c1da1
Adrian Rutkowski <adrian.rutkowsky@gmail.com>
2014-04-16 19:15:52 +0200

    Add handling file upload and basic import students from csv
    Change-Id: I4a7ab7690a3c24c3fda94eed3ae5419b463037ba

--- src/apps/panel/views.py
+++ src/apps/panel/views.py
@@ -1,7 +1,12 @@
+ #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.views.generic import FormView
 from django.contrib.auth.models import User
 from apps.plan.models import Group
 from apps.panel.forms import ImportCSVForm
+import csv
+import os
 
 class ImportStudentsView(FormView):
     template_name = 'panel/import_students.html'
@@ -9,5 +14,37 @@ class ImportStudentsView(FormView):
     success_url = '/'
     
     def form_valid(self, form):
-        # Handle import students
+        uploaded_file = form.cleaned_data['file']
+        handle_uploaded_file(uploaded_file)
+        with open('uploads/' + uploaded_file.name) as csvfile:
+            import_students_from_csv(csvfile)
         return super(ImportStudentsView, self).form_valid(form)
+
+def handle_uploaded_file(uploaded_file):
+    """
+        Save uploaded file
+    """
+    filename = uploaded_file.name
+    with open('uploads/' + filename, 'wb+') as destination:
+        for chunk in uploaded_file.chunks():
+            destination.write(chunk)
+            
+def remove_uploaded_file(uploaded_file):
+    """
+        Remove uploaded file from server
+    """
+    filename = uploaded_file.name
+    os.remove('uploads/' + filename)
+
+def import_students_from_csv(csvfile):
+    """
+        Import students objects from csvfile. Accepts file object as parameter (open file)
+        :param csvfile: file
+    """
+    students_reader = csv.DictReader(csvfile)
+    for row in students_reader:
+        if User.objects.filter(username=row['Indeks']).exists() == False:
+            user = User.objects.create(username=row['Indeks'])
+            password = User.objects.make_random_password()
+            user.set_password(password)
+            user.save()
