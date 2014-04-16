36f8763a64a603805aa956d0220a47b688ff40bc
Piotr Palkiewicz <piotrpalkiewicz@gmail.com>
2014-04-16 15:13:09 +0200

    Login and logout
    Change-Id: I978e31fb4b94844193ab7373ed52c99017468689

--- src/degra/urls.py
+++ src/degra/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^password/', include('password_reset.urls')),
     url(r'^', include('apps.plan.urls')),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
 )
