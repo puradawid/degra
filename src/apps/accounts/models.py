145529d5d55fd33aae69691666400fee059cd3b1
Paweł Kondraciuk <kondraciukpawel@gmail.com>
2014-04-27 00:21:07 +0200

    Add grab index number method
    Change-Id: I34d3d845267d7aee11cac2648e5459bd4eb7d956

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from apps.plan.models import Group
+from django.db.models.signals import post_save
+import re
 
 class Account(models.Model):
     user = models.OneToOneField(User, related_name='profile')
@@ -8,3 +10,17 @@ class Account(models.Model):
     
     def __unicode__(self):
         return self.user.username
+    
+def create_account_for_new_user(sender, created, instance, **kwargs):
+    if created:
+        account = Account.objects.create(user=instance)
+        
+post_save.connect(create_account_for_new_user, User, dispatch_uid='create_account_for_new_user')
+
+def get_index_number(self):
+    m = re.search('(?!=[a-zA-Z])\d+$', self.username)
+    if m:
+        return m.group()
+    return self.username
+
+User.add_to_class("get_index_number", get_index_number)
