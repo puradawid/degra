5f991850f2c87f5a6f67089aa9c96d8295afa847
Paweł Kondraciuk <kondraciukpawel@gmail.com>
2014-04-28 13:50:37 +0200

    Show personalized lesson list
    Change-Id: I804bbedcfdde1d09568e726a6bdd58ed16e6d466

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -2,7 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from apps.plan.models import Group
 from django.db.models.signals import post_save
+from apps.plan.models import Lesson, Note
 import re
+
+class UserToLesson(models.Model):
+    user = models.OneToOneField(User)
+    lesson = models.OneToOneField(Lesson)
+    notes = models.ManyToManyField(Note)
 
 class Account(models.Model):
     user = models.OneToOneField(User, related_name='profile')
@@ -11,16 +17,16 @@ class Account(models.Model):
     def __unicode__(self):
         return self.user.username
     
-def create_account_for_new_user(sender, created, instance, **kwargs):
+def create_profile(sender, created, instance, **kwargs):
     if created:
-        account = Account.objects.create(user=instance)
+        Account.objects.create(user=instance)
         
-post_save.connect(create_account_for_new_user, User, dispatch_uid='create_account_for_new_user')
+post_save.connect(create_profile, User, dispatch_uid='create_profile')
 
 def get_index_number(self):
     m = re.search('(?!=[a-zA-Z])\d+$', self.username)
     if m:
         return m.group()
-    return self.username
+    return "0"
 
 User.add_to_class("get_index_number", get_index_number)
--- src/apps/plan/views.py
+++ src/apps/plan/views.py
@@ -1,12 +1,21 @@
 from django.views.generic.list import ListView
 from apps.plan.models import Lesson
+from apps.accounts.models import UserToLesson
 
 class PlanView(ListView):
     template_name = 'plan/plan.html'
     context_object_name = 'lesson_list'
     
     def get_queryset(self):
-        return Lesson.objects.all()
+        if self.request.user.is_authenticated:
+            lessonlist = list()
+            utllist = UserToLesson.objects.filter(user=self.request.user)
+            for obj in utllist:
+                lesson = obj.lesson
+                lesson.notes = obj.notes
+                lessonlist.append(lesson)
+            
+            return lessonlist
 
     def get_context_data(self, **kwargs):
         context = super(PlanView, self).get_context_data(**kwargs)
