557eeeb598cd429b9e16608169bc09258d7db80e
Paweł Kondraciuk <kondraciukpawel@gmail.com>
2014-04-29 14:39:07 +0200

    Add index number parser
    Change-Id: I8b53c1fd3829b05cafb715d90a16e662103a731e

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -4,29 +4,33 @@ from apps.plan.models import Group
 from django.db.models.signals import post_save
 from apps.plan.models import Lesson, Note
 import re
+from django.core.exceptions import ValidationError
 
 class UserToLesson(models.Model):
-    user = models.OneToOneField(User)
-    lesson = models.OneToOneField(Lesson)
+    user = models.ForeignKey(User)
+    lesson = models.ForeignKey(Lesson)
     notes = models.ManyToManyField(Note)
 
 class Account(models.Model):
+    index_number = models.IntegerField(primary_key=True)
     user = models.OneToOneField(User, related_name='profile')
     groups = models.ManyToManyField(Group)
     
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            m = re.search('(?!=[a-zA-Z])\d+$', self.user.username)
+            if m:
+                self.index_number = m.group()
+            else:
+                raise ValidationError('Not a valid index number!')
+        
+        super(Account, self).save(*args, **kwargs)
+
     def __unicode__(self):
         return self.user.username
-    
+
 def create_profile(sender, created, instance, **kwargs):
     if created:
-        Account.objects.create(user=instance)
+        Account.objects.get_or_create(user=instance)
         
 post_save.connect(create_profile, User, dispatch_uid='create_profile')
-
-def get_index_number(self):
-    m = re.search('(?!=[a-zA-Z])\d+$', self.username)
-    if m:
-        return m.group()
-    return "0"
-
-User.add_to_class("get_index_number", get_index_number)
