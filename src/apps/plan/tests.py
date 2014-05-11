82d006029241728f378c3cd5d87cec01ba93eef3
Paweł Kondraciuk <kondraciukpawel@gmail.com>
2014-05-11 15:13:12 +0200

    Correct fixtures
    Change-Id: Id415875bff051b8d17860456706d41d5239691a9

--- src/apps/plan/tests.py
+++ src/apps/plan/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.core.management import call_command
 from apps.accounts.models import UserToLesson
+from apps.plan.modeldir.lesson import Lesson
+from django.contrib.auth.models import User
 
 class PlanTestCase(TestCase):
     
@@ -8,15 +10,18 @@ class PlanTestCase(TestCase):
         # Load fixtures
         call_command('loaddata', 'account', verbosity=0)
         call_command('loaddata', 'plan', verbosity=0)
+        call_command('loaddata', 'usertolesson', verbosity=0)
+        
+        UserToLesson.objects.create(user=User.objects.get(pk=2), lesson=Lesson.objects.get(pk=2))
 
     def test_notes_relations(self):
-        user2_notes = UserToLesson.objects.get(lesson__pk=2, user__pk=1).notes
+        user2_notes = UserToLesson.objects.get(lesson__pk=2, user__pk=2).notes
         self.assertEqual(user2_notes.count(), 0)
-        user1_notes = UserToLesson.objects.get(lesson__pk=2, user__pk=2).notes
+        user1_notes = UserToLesson.objects.get(lesson__pk=2, user__pk=1).notes
         self.assertEqual(user1_notes.count(), 1)
-        
+
     def test_lesson_relations(self):
-        user2_lesson = UserToLesson.objects.filter(user__pk=1)
+        user2_lesson = UserToLesson.objects.filter(user__pk=2)
         self.assertEqual(user2_lesson.count(), 1)
-        user1_lesson = UserToLesson.objects.filter(user__pk=2)
+        user1_lesson = UserToLesson.objects.filter(user__pk=1)
         self.assertEqual(user1_lesson.count(), 3)
