6b13b12940fafef938dd50772469c9ca21a8ed82
Paweł Kondraciuk <kondraciukpawel@gmail.com>
2014-05-13 10:52:46 +0200

    Add get plan function for user, correct fixtures, add transfer model
    Change-Id: I1413a857653cd1ad0e80b9b6a33125b7afcdce8d

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from apps.plan.models import Group
 from django.db.models.signals import post_save
-from apps.plan.models import Lesson, Note
+from apps.plan.models import Lesson
 import re
 from django.core.exceptions import ValidationError
+from django.db.models import Q
 
 class Account(models.Model):
     index_number = models.CharField(primary_key=True, max_length=7)
@@ -13,6 +14,48 @@ class Account(models.Model):
     
     def __unicode__(self):
         return self.index_number
+    
+    def update_group(self, group, group_prefix):
+        # check if user is already in such type of group
+        try:
+            self.groups.filter(name__regex='^%s' % group_prefix).delete()
+        except Group.DoesNotExist:
+            pass
+        
+        self.groups.add(group)
+        self.groups.save()
+    
+    def get_plan(self):
+        # get transfer list
+        transfer_list = StudentTransfer.objects.filter(account=self)
+
+        # include transfered lessons
+        lesson_list = Lesson.objects.filter(Q(group__in=self.groups.all()) | Q(id__in=transfer_list.values_list('target', flat=True)), ~Q(id__in=transfer_list.values_list('origin', flat=True)))
+        
+        # exclude origin lessons
+        return lesson_list
+        
+    def make_transfer(self, old_lesson, new_lesson):
+        
+        if old_lesson == new_lesson:
+            return
+
+        try:
+            # check if lesson is already transfered
+            transfer = StudentTransfer.objects.get(target=old_lesson, account=self)
+            
+            # if transfer exists origin is transfer origin, l1 -1> l2 -2> l3
+            # that means after second transfer new transfer should point to origin lesson l1 -> l3 not l2 -> l3
+            origin = transfer.origin
+            # if new lesson is same as origin lesson remove transfer
+            if new_lesson == origin:
+                transfer.delete()
+                return
+            
+        except StudentTransfer.DoesNotExist:
+            origin = old_lesson
+        
+        StudentTransfer.objects.create(account=self, origin=origin, target=new_lesson)
 
 def create_profile(sender, created, instance, **kwargs):
     if created:
@@ -24,10 +67,10 @@ def create_profile(sender, created, instance, **kwargs):
             account[0].save()
         else:
             raise ValidationError('Not a valid index number!')        
-        
+
 post_save.connect(create_profile, User, dispatch_uid='create_profile')
 
-class UserToLesson(models.Model):
-    account = models.ForeignKey(Account, blank=True, null=True)
-    lesson = models.ForeignKey(Lesson)
-    notes = models.ManyToManyField(Note)
+class StudentTransfer(models.Model):
+    account = models.ForeignKey(Account)
+    origin = models.ForeignKey(Lesson, related_name='origin')
+    target = models.ForeignKey(Lesson, related_name='target')
