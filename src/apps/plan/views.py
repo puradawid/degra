7a3a711c7533137384240070e5a61834d6495440
Paweł Kondraciuk <kondraciukpawel@gmail.com>
2014-05-13 10:53:40 +0200

    Correct data processin
    Change-Id: I4662e578b62193be69753b0e15fea9847ce5bd3e

--- src/apps/plan/views.py
+++ src/apps/plan/views.py
@@ -1,32 +1,45 @@
 from django.views.generic.list import ListView
 from apps.plan.models import Lesson, Post
-from apps.accounts.models import UserToLesson
+from apps.accounts.models import Account
 from django.db.models import Q
+from apps.plan.modeldir.note import Note
+
+def get_groups_plan(field, semestr, groups, electives=None):
+    """
+        Returns plan for a selected groups
+    """
+    qgroups = Q()
+    qelectives = Q()
+    
+    for value in groups:
+        qgroups |= Q(group__name__iexact = value, group__field_of_study__iexact=field, group__semestr=semestr)
+
+    if electives is not None:
+        for value in electives:
+            qelectives |= Q(group__name__iexact = value, group__semestr=0)
+
+    return Lesson.objects.filter(qgroups | qelectives)
 
 class PersonalizedPlanView(ListView):
     template_name = 'plan/plan.html'
     context_object_name = 'lesson_list'
     
+    def complete_plan(self, plan):
+        for lesson in plan:
+            lesson.notes = Note.objects.filter(lesson=lesson, author=self.request.user.profile)
+            print lesson.notes
+        return plan
+    
     def get_queryset(self):
-        lesson_list = list()
 
         if 'index_number' in self.kwargs:
-            utllist = UserToLesson.objects.filter(account__index_number=self.kwargs['index_number'])
+            profile = Account.objects.get(index_number=self.kwargs['index_number'])
         elif self.request.user.is_authenticated():
-            utllist = UserToLesson.objects.filter(account=self.request.user.profile)
-            
-        else: # tymczasowe rozwiazanie
-            pview = PlanView()
-            pview.kwargs={'field': 'inf', 'semestr': '1', 'groups': 'cw1/wyk1'}
-            return pview.get_queryset()
+            profile = self.request.user.profile
+        else:
+            return get_groups_plan('INF', '1', ('cw1', 'ps1'))
 
-        for obj in utllist:
-            lesson = obj.lesson
-            if self.request.user.is_authenticated() and obj.user == self.request.user:
-                lesson.notes = obj.notes
-            lesson_list.append(lesson)
-
-        return lesson_list
+        return self.complete_plan(profile.get_plan())
 
     def get_context_data(self, **kwargs):
         context = super(PersonalizedPlanView, self).get_context_data(**kwargs)
@@ -41,6 +54,12 @@ class PlanView(ListView):
     template_name = 'plan/plan.html'
     context_object_name = 'lesson_list'
     
+    def complete_plan(self, plan):
+        for lesson in plan:
+            lesson.notes = Note.objects.filter(lesson=lesson, author=self.request.user.profile)
+        
+        return plan
+    
     def get_queryset(self):
         qgroups = Q()
         qelectives = Q()
@@ -48,6 +67,12 @@ class PlanView(ListView):
         field = self.kwargs['field']
         semestr = self.kwargs['semestr']
         groups = self.kwargs['groups'].split('/')
+        
+        if "electives" in self.kwargs:
+            electives = self.kwargs['electives'].split('/')
+            return get_groups_plan(field, semestr, groups, electives)
+        else:
+            return get_groups_plan(field, semestr, groups)
         
         for value in groups:
             qgroups |= Q(group__name__iexact = value, group__field_of_study__iexact=field, group__semestr=semestr)
