83a6e7fefee641ada61c9d3955d480db67faeb93
Adrian Rutkowski <adrian.rutkowsky@gmail.com>
2014-05-30 22:32:36 +0200

    Register models and add stuff like filters, search fields etc.
    Change-Id: I4b41cbf836fd28148699d7166d65fc91e68a7ab8

--- src/apps/accounts/admin.py
+++ src/apps/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.accounts.models import Account
 
-# Register your models here.
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('index_number',)
+    list_filter = ('groups',)
+    search_fields = ['index_number']
+
+admin.site.register(Account, AccountAdmin)
