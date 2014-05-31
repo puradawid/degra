a1e699c256acc7f73c61da63454df358a44c72ad
Adrian Rutkowski <adrian.rutkowsky@gmail.com>
2014-05-31 22:45:44 +0200

    Add models translations
    Change-Id: Id3bb06f8f4e2d2b9af2c053322dee880638224d0

--- src/apps/plan/modeldir/post.py
+++ src/apps/plan/modeldir/post.py
@@ -1,12 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length = 255)
-    content = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length = 255, verbose_name = 'Tytuł')
+    content = models.TextField(verbose_name = 'Treść')
+    created = models.DateTimeField(auto_now_add=True, verbose_name = 'Utworzono')
     
     class Meta:
         app_label = "plan"
+        verbose_name = 'Ogłoszenie'
+        verbose_name_plural = 'Ogłoszenia'
     
     def __unicode__(self):
         return self.title
