3a654bfe2da07933948eb8756a2aa693491400ed
Adrian Rutkowski <adrian.rutkowsky@gmail.com>
2014-06-03 19:05:04 +0200

    Merge admin-panel
    Change-Id: Ibd5aa6d795a2d6a8d73c729b7823e061e5d7bf6a

--- src/apps/plan/modeldir/course.py
+++ src/apps/plan/modeldir/course.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class Course(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length = 100, verbose_name = 'Nazwa')
     
     class Meta:
         app_label = "plan"
+        verbose_name = 'Kurs'
+        verbose_name_plural = 'Kursy'
     
     def __unicode__(self):
         return self.name
